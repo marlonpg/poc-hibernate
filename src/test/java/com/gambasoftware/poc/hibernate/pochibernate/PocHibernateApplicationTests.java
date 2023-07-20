3d4100db5cb096819d09156623a5e5d9ca83e6b3
Marlon Gamba <marlonpgamba@gmail.com>
2023-07-20 15:24:08 -0300

    Adding template code for poc

---
+++ src/test/java/com/gambasoftware/poc/hibernate/pochibernate/PocHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gambasoftware.poc.hibernate.pochibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
