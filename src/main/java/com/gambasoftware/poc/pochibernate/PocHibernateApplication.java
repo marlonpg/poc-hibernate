ca9615e886ce21320209cac34d632d8b65b55f86
Marlon Gamba <marlonpgamba@gmail.com>
2023-07-20 15:51:57 -0300

    Update with correct template.

---
+++ src/main/java/com/gambasoftware/poc/pochibernate/PocHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.gambasoftware.poc.pochibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocHibernateApplication.class, args);
+	}
+
+}
